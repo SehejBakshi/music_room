861cbe3812982ce16bdc4aad390bb66b5377b991
SehejBakshi <sehej.bakshi@yahoo.in>
2021-05-06 15:08:23 +0530

    Add Settings for Room Host

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import ( RoomView, CreateRoomView, 
                     GetRoom, JoinRoom, UserInRoom,
-                    LeaveRoom)
+                    LeaveRoom, UpdateRoom)
 
 urlpatterns = [
     path('room', RoomView.as_view()),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('join-room', JoinRoom.as_view()),
     path('user-in-room', UserInRoom.as_view()),
     path('leave-room', LeaveRoom.as_view()),
+    path('update-room', UpdateRoom.as_view()),
 ]
